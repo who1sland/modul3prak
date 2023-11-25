ddd449ed61b8ba0dc7e6a7d3e670dafee0cb2799
feldamfh <feldamfh@wembail.umm.ac.id>
2023-11-25 21:51:58 +0700

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        String text = "StringUtils.reverse(Text)";
+        String reverse = "Hello maven!";
+        System.out.println(reverse);
+    }
+}
